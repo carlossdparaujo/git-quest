291c0c0b5ab378c011d2ffdbd43fdaf07f48f6b6
Carlos Araujo <caraujo@thoughtworks.com>
2017-03-21 01:28:44 -0300

    Git-quest has loop for reading commands

--- git_quest.go
+++ git_quest.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"strings"
+	"os"
 	"git-quest/quest"
 	"git-quest/quest/commands"
 )
@@ -14,6 +17,24 @@ func main() {
 
 	fmt.Printf("%s\n", quest.Description())
 
-	_, message := quest.Check(nil)
-	fmt.Printf("%s\n", message)
+	text := ""
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		text, _ = reader.ReadString('\n')
+		text = strings.Trim(text, "\n")
+
+		if (text == "exit") {
+			break
+		}
+
+		command := commands.NewTerminalCommandFromText(text)
+
+		_, message := quest.Check(command)
+		fmt.Printf("%s\n", message)
+
+		if (quest.Completed()) {
+			break
+		}
+	}
 }
